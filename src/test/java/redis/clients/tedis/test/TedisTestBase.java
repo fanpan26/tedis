afaa524622450789ed7d8986edfca16881c935f2
fanpan26 <fanpan26@126.com>
2019-01-23 20:13:56 +0800

    test base

--- src/test/java/redis/clients/tedis/test/TedisTestBase.java
+++ src/test/java/redis/clients/tedis/test/TedisTestBase.java
@@ -2,11 +2,13 @@ package redis.clients.tedis.test;
 
 import redis.clients.tedis.Tedis;
 
+
 public class TedisTestBase {
-    protected Tedis tedis= new Tedis("192.168.1.225", 6379);
+    protected Tedis tedis= new Tedis("192.168.187.129", 6379);
 
     protected String generateKey(String prefix) {
-        return "tedis:" + prefix + ":" + System.currentTimeMillis();
+        long ran = tedis.incrby("ranKey",1);
+        return prefix+"_"+ ran;
     }
 
 }
